a5dffb4f185facb8dda9f837833a2af88f280c2b
MathiasAmi <42993917+MathiasAmi@users.noreply.github.com>
2018-09-17 14:31:04 +0200

    Rettede fejlen
    Fandt fejlen og rettede den

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import java.awt.*;
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Main {
-    public static void main (String[]args){
-        System.out.println("Hello World");
-        System.out.println("Mathias");
+
+    public static void main (String[] args) {
+        Dog d = new Dog("Max", 3);
+
+        d.bark();
+
+        int dogYears = d.getDogYears();
+        System.out.print(dogYears + " dog years.");
+
+        d.toy();
+        d.toy();
+        d.toy();
+        d.toy();
+
     }
-}
+
+    }
